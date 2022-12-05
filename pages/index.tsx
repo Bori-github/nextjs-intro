987106d13c472b66e3f265ecfa6f3722277d1911
Bori-github <qhflrnfl4324@gmail.com>
2022-12-05 16:44:57 +0900

    🔧 rewrites를 이용하여 api destination 마스킹 처리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Title } from '../components/Title';
 
-const API_KEY = '10923b261ba94d897ac6b81148314a3f';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch('/api/movies')).json();
       setMovies(results);
     })();
   }, []);
