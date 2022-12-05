b473866057de628c2e5d7975b1a900adabd4d72c
Bori-github <qhflrnfl4324@gmail.com>
2022-12-05 15:48:59 +0900

    ✨ themoviedb 데이터 fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../components/Title';
 
+const API_KEY = '10923b261ba94d897ac6b81148314a3f';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
 const HomePage = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Title title="Home" />
-      <h1>Hi</h1>
+      {!movies.length && <h4>Loading</h4>}
+      {movies?.map((movie) => {
+        const { id, original_title, poster_path } = movie;
+        return (
+          <div key={id}>
+            <img src={`${IMAGE_BASE_URL}${poster_path}`} alt="" />
+            <h4 key={id}>{original_title}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
