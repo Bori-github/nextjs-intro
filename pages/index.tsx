b4b57e227fe64f0b2283be9a73cb4758dbc7badf
Bori-github <qhflrnfl4324@gmail.com>
2022-12-05 19:24:10 +0900

    ⚡️ data fetching SSR 방식으로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
 import { Title } from '../components/Title';
 
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
-const HomePage = () => {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch('/api/movies')).json();
-      setMovies(results);
-    })();
-  }, []);
+interface MovieProps {
+  id: number;
+  original_title: string;
+  poster_path: string;
+}
+interface ResultsProps {
+  results: MovieProps[];
+}
 
+const HomePage = ({ results }: ResultsProps) => {
   return (
     <div>
       <Title title="Home" />
-      {!movies.length && <h4>Loading</h4>}
-      {movies?.map((movie) => {
+      {results?.map((movie) => {
         const { id, original_title, poster_path } = movie;
         return (
           <div key={id}>
@@ -27,6 +27,13 @@ const HomePage = () => {
       })}
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { results } = await (
+    await fetch('http://localhost:3000/api/movies')
+  ).json();
+  return { props: { results } };
 };
 
 export default HomePage;
